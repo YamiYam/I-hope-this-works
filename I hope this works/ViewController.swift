f4a24dc3cc5380a256194dc59af324dd047a10e4
Ian Hatlewick <ianhatlewick@ians-mac-pro.local>
2017-12-14 15:26:24 -0600

    test commit

--- I hope this works/ViewController.swift
+++ I hope this works/ViewController.swift
@@ -8,8 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
+   
+    @IBOutlet weak var CityTextBox: UITextField!
+    @IBOutlet weak var CityDataWheel: UIPickerView!
+    
+    
+    var cities = ["Ankeny","Des Moines","Cedar Rapids","Ames","Waterloo","England"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        let countrows: Int = cities.count
+        
+        return countrows
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let titleRow = cities[row]
+        
+        return titleRow
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.CityTextBox.text = cities[row]
+        self.CityDataWheel.isHidden = true
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.CityDataWheel.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
